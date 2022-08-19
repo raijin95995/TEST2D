e66c106e814a2785f6e1afe0d94baf6efaecd71c
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-08-19 20:50:25 +0800

    對話

---
+++ Assets/scripts/TalkUI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// ¹ï¸Ü®Ø¼u¥X
+/// </summary>
+public class TalkUI : MonoBehaviour
+{
+    public GameObject talkMessage;
+    public GameObject talkUI;
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (talkMessage.activeSelf && Input.GetKeyDown(KeyCode.R))
+        {
+            talkUI.SetActive(true);
+            Invoke("DisplayUI", 6.0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "player")
+        {
+            talkMessage.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "player")
+        {
+            talkMessage.SetActive(false);
+        }
+    }
+
+    void DisplayUI()
+    {
+        talkUI.SetActive(false);
+    }    
+}
