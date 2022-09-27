7c23a9eba2ddcff2a48557a888f714d912ef62ff
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-09-27 16:41:12 +0800

    斬擊圖片

--- Assets/scripts/ImageLive.cs
+++ Assets/scripts/ImageLive.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ImageLive : MonoBehaviour
 {
+    public Image img;
+
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("Display", 0.5f);
+        //Invoke("Display", 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        img.color -= new Color(0, 0, 0, 0.01f);
     }
 
 
